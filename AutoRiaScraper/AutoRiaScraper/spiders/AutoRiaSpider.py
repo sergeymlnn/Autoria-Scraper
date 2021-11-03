0f9318f93f9cc3ef6733988e64aa9af62499289e
sergeymlnn <malxao666@gmail.com>
2021-11-03 23:40:42 +0200

    Added a feature to manage input arguments of the spider based on the SpiderArguments model in items.py

--- AutoRiaScraper/AutoRiaScraper/spiders/AutoRiaSpider.py
+++ AutoRiaScraper/AutoRiaScraper/spiders/AutoRiaSpider.py
@@ -1,10 +1,24 @@
-import scrapy
+from typing import *
+
+from scrapy import Spider
+from scrapy.http.response.html import HtmlResponse
+
+from AutoRiaScraper.items import SpiderArguments
 
 
-class AutoriaSpider(scrapy.Spider):
-    name = 'autoria_spider'
-    allowed_domains = ['auto.ria.com']
-    start_urls = ['https://auto.ria.com/uk/']
+class AutoriaSpider(Spider):
+  """Spdier to parse information about cars, based on specified filters from the main page"""
+  name = 'autoria_spider'
+  allowed_domains = ['auto.ria.com']
+  start_urls = ['https://auto.ria.com/uk/']
 
-    def parse(self, response):
-        pass
+  def __init__(self, *args, **kwargs):
+    """"""
+    super().__init__(*args, **kwargs)
+    self.args = SpiderArguments(**kwargs)
+
+  def parse(self, response: HtmlResponse) -> None:
+    """"""
+    yield {
+      "status": "OK",
+    }
