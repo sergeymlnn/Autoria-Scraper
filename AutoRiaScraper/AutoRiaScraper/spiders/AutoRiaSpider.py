5bb7fdb04bc59768e44bcdc07ee447409daff7fa
sergeymlnn <malxao666@gmail.com>
2021-11-04 18:49:07 +0200

    Updated code style and import statements

--- AutoRiaScraper/AutoRiaScraper/spiders/AutoRiaSpider.py
+++ AutoRiaScraper/AutoRiaScraper/spiders/AutoRiaSpider.py
@@ -1,4 +1,4 @@
-from typing import *
+from typing import Iterator, Dict
 
 from scrapy import Spider
 from scrapy.http.response.html import HtmlResponse
@@ -7,18 +7,18 @@ from AutoRiaScraper.items import SpiderArguments
 
 
 class AutoriaSpider(Spider):
-  """Spdier to parse information about cars, based on specified filters from the main page"""
-  name = 'autoria_spider'
-  allowed_domains = ['auto.ria.com']
-  start_urls = ['https://auto.ria.com/uk/']
+    """Spdier to parse information about cars, based on specified filters from the main page"""
+    name = 'autoria_spider'
+    allowed_domains = ['auto.ria.com']
+    start_urls = ['https://auto.ria.com/uk/']
 
-  def __init__(self, *args, **kwargs):
-    """"""
-    super().__init__(*args, **kwargs)
-    self.args = SpiderArguments(**kwargs)
+    def __init__(self, *args, **kwargs):
+        """"""
+        super().__init__(*args, **kwargs)
+        self.args = SpiderArguments(**kwargs)
 
-  def parse(self, response: HtmlResponse) -> None:
-    """"""
-    yield {
-      "status": "OK",
-    }
+    def parse(self, response: HtmlResponse) -> Iterator[Dict[str, str]]:
+        """"""
+        yield {
+            "status": "OK",
+        }
