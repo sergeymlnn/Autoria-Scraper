2d0d0afbcdfcc922bf5d323cdcbdbd092968db0d
sergeymlnn <malxao666@gmail.com>
2021-11-18 19:49:43 +0200

    Removed unnecessary comment

--- AutoRiaScraper/AutoRiaScraper/items.py
+++ AutoRiaScraper/AutoRiaScraper/items.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import Any, Dict, Optional, Literal, List
 from urllib.parse import urljoin
 
-from itemloaders.processors import Join, MapCompose
+from itemloaders.processors import Join, MapCompose, TakeFirst
 from scrapy import Item as ScrapyItem, Field as ScrapyField
 from pydantic import BaseModel, Field, conint, validator
 from scrapy.loader import ItemLoader
@@ -12,6 +12,15 @@ MAX_YEAR = datetime.now().year
 MIN_YEAR = MAX_YEAR - 10
 
 MIN_PRICE = 0
+
+
+# Wrapper for ScrapyField, used on string fields, that returns the first valid
+# value and eventually strips it.
+DefaultStringField = lambda default = "": ScrapyField(
+    default,
+    input_processor=MapCompose(str.strip),
+    output_processor=TakeFirst()
+)
 
 
 class SpiderArguments(BaseModel):
@@ -49,6 +58,62 @@ class SpiderArguments(BaseModel):
 
 class CarItemsOnCategoryPage(ScrapyItem):
     """Item to extract car links on a category page and link to the next page"""
-    current_page_url: str = ScrapyField("", output_processor=Join())
-    next_page_url: str = ScrapyField("", output_processor=Join())
+    current_page_url: str = DefaultStringField()
+    next_page_url: str = DefaultStringField()
     car_urls_on_page: List[str] = ScrapyField(default=[])
+
+
+class CarSellerItem(ScrapyItem):
+    """Item to extract information about seller of a particular car"""
+    name: str = DefaultStringField()
+    last_online_time: str = DefaultStringField()
+    location: str = DefaultStringField()
+    verified_by_bank: bool = ScrapyField(default=False)
+    phone_verified: bool = ScrapyField(default=True)
+    signed_in_date: str = DefaultStringField()
+    reputation: float = ScrapyField(default=0.0)
+    total_clients_server: int = ScrapyField(default=0)
+
+
+class CarSaleAdItem(ScrapyItem):
+    """Item to extract additional information about car sale ad"""
+    id: str = DefaultStringField()
+    created_at: str = DefaultStringField()
+    views: int = ScrapyField(default=0)
+    saved: int = ScrapyField(default=0)
+
+
+class CarItem(ScrapyItem):
+    """Item to extract full information about a car"""
+    link: str = DefaultStringField()
+    brand: str = DefaultStringField()
+    model: str = DefaultStringField()
+    year: str = DefaultStringField()
+    color: str = DefaultStringField()
+    engine_capacity: str = DefaultStringField()
+    last_repair: str = DefaultStringField()
+    last_repair_info: str = DefaultStringField()
+    total_owners: int = ScrapyField(default=1)
+    remains_at_large: bool = ScrapyField(default=False)
+    encumbrance_type: str = DefaultStringField()
+    exclusion_limitations: str = DefaultStringField()
+    official_mileage: float = ScrapyField(default=0.0)
+    mileage_declared_by_seller: float = ScrapyField(default=0.0)
+    mileage_fixation_date: str = DefaultStringField()
+    mileage_fixation_source: str = DefaultStringField()
+    has_crashes: bool = ScrapyField(default=False)
+    crash_info: str = DefaultStringField()
+    car_public_number: str = DefaultStringField()
+    VIN: str = DefaultStringField()
+    is_VIN_confirmed: bool = ScrapyField(default=False)
+    tags: List[str] = ScrapyField(default=[])
+    images: List[str] = ScrapyField(default=[])
+    body_type: str = DefaultStringField()
+    transmission_type: str = DefaultStringField()
+    drive_unit: str = DefaultStringField()
+    description: str = DefaultStringField()
+    safety: str = DefaultStringField()
+    comfort: str = DefaultStringField()
+    multimedia: str = DefaultStringField()
+    price: str = DefaultStringField()
+    condition: str = DefaultStringField()
