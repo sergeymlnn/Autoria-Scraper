66ad04324634a11fa75cb16af0bc0a92db11cb78
sergeymlnn <malxao666@gmail.com>
2021-11-18 19:47:14 +0200

    Defined all item loaders to be used for the scraping

--- AutoRiaScraper/AutoRiaScraper/spiders/AutoRiaSpider.py
+++ AutoRiaScraper/AutoRiaScraper/spiders/AutoRiaSpider.py
@@ -3,15 +3,16 @@ from pprint import pprint
 
 from scrapy import Spider, Request
 from scrapy.http.response.html import HtmlResponse
+from scrapy.loader import ItemLoader
 from scrapy_splash import SplashRequest
 
-from AutoRiaScraper.items import SpiderArguments
+from AutoRiaScraper.items import SpiderArguments, CarItemsOnCategoryPage
 
 
 lua_script = """
     function main(splash, args)
         assert(splash:go(args.url))
-        assert(splash:wait(2))
+        assert(splash:wait(3))
         return splash:html()
     end
 """
@@ -21,7 +22,7 @@ class AutoriaSpider(Spider):
     """Spdier to parse information about cars, based on specified filters from the main page"""
     name = 'autoria_spider'
     allowed_domains = ['auto.ria.com']
-    start_urls = ['https://auto.ria.com/uk/']
+    start_urls = ['https://auto.ria.com/uk/bus/?page=1']
 
     def __init__(self, *args, **kwargs):
         """"""
@@ -49,6 +50,19 @@ class AutoriaSpider(Spider):
 
     def parse(self, response: HtmlResponse) -> Iterator[Dict[str, str]]:
         """Iterates over car items and performs requests to each individual car page"""
-        yield {
-            "status": "OK",
-        }
+        item = ItemLoader(item=CarItemsOnCategoryPage(), response=response)
+        item.add_value("current_page_url", response.url)
+        item.add_xpath("next_page_url", "//a[@class='page-link js-next ']/@href")
+        item.add_xpath("car_urls_on_page", "//div[@class='item ticket-title']/a/@href")
+        loaded_item = item.load_item()
+
+        for car_url in loaded_item.car_urls_on_page:
+            pass
+
+        if loaded_item.next_page_url:
+            yield Request(loaded_item.next_page_url, callback=self.parse)
+
+
+    def parse_car_page(self, response: HtmlResponse):
+        """"""
+        pass
