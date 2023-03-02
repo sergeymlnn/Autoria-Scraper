f923b89a3847221157b73121719120dfbf3d1812
sergeymlnn <malxao666@gmail.com>
2023-03-02 22:07:46 +0200

    Updated method to collect info about specific car: added logging & attrs of a car item

--- AutoRiaScraper/AutoRiaScraper/spiders/AutoRiaSpider.py
+++ AutoRiaScraper/AutoRiaScraper/spiders/AutoRiaSpider.py
@@ -101,8 +101,8 @@ class AutoRiaSpider(Spider):
           endpoint="execute",
           args={"lua_source": self.lua_car_page_script},
           cache_args=["lua_source"],
-          callback=self.parse_car_page,
-          meta=meta
+          callback=self.parse_car,
+          meta=meta,
         )
 
       # Pagination
@@ -112,121 +112,27 @@ class AutoRiaSpider(Spider):
         args={"lua_source": self.lua_category_page_script},
         cache_args=["lua_source"],
         callback=self.parse_cars,
-        meta=meta
+        meta=meta,
       )
 
-    def parse_car_page(self, response: HtmlResponse) -> None:
-        """Collects full information about car and its seller on the car page"""
-        # inspect_response(response, self)
-        car_item = ItemLoader(item=CarItem(), response=response)
-        car_item.add_value("link", response.url)
-        car_item.add_xpath("model", "//h1[@class='head']/text()")
-        car_item.add_xpath("brand", "//h1[@class='head']/span/text()")
-        car_item.add_xpath("year", "//h1[@class='head']/text()")
-        car_item.add_xpath("price", "//div[@class='price_value']/strong/text()")
-        car_item.add_xpath("description", "//div[@class='full-description']/text()")
+    def parse_car(self, response: HtmlResponse) -> None:
+      """
+      Collects info about specific car.
 
-        # Each <dd> element contains 2 <span> elements inside
-        # 1-st <span> with class 'label' is a spec name
-        # 2-nd <span> with class 'argument' is a spec value
-        technical_info = response.xpath("//div[contains(@class, 'technical-info')]/dl/dd")
-        tech_cert_info = technical_info.xpath("./div[@class='t-check']")
-
-        # Contains specifications of the car following the pattern: <spec_name>: <spec_value>
-        technical_attr = "./span[@class='label']/text()"
-        technical_attr_value = "./span[contains(@class, 'argument')]//text()"
-        car_info = {
-            spec.xpath(technical_attr).get(): " ".join(spec.xpath(technical_attr_value).getall())
-            for spec in technical_info
-        }
-
-        description_specs = response.xpath("//div[@id='description_v3']/dl/dd")
-        spec_attr_xpath = "./span[@class='label']/text()"
-        spec_attr_value_xpath = "./span[@class='argument']//text()"
-        additional_specs = {
-            spec.xpath(spec_attr_xpath).get(): " ".join(spec.xpath(spec_attr_value_xpath).getall())
-            for spec in description_specs
-        }
-        car_info.update(additional_specs)
-
-        has_crashes = car_info.get("Участь в ДТП", "").lower() == "був в дтп"
-        remains_at_large = not car_info.get("В розшуку", "").lower() == "ні"
-        pb_number = tech_cert_info.xpath("./span[@class='state-num ua']/text()").get()
-        vin_info = tech_cert_info.xpath("./span[@class='checked_ad label-check']/text()").get("")
-        is_vin_number_confirmed = "Перевірений VIN-код" in vin_info
-        vin_number = tech_cert_info.xpath("./span[@class='label-vin']/text()").get()
-
-        car_item.add_value("color", car_info.get("Колір"))
-        car_item.add_value("engine_capacity", car_info.get("Двигун"))
-        car_item.add_value("mileage_declared_by_seller", car_info.get("Пробіг"))
-        car_item.add_value("multimedia", car_info.get("Мультимедіа"))
-        car_item.add_value("comfort", car_info.get("Комфорт"))
-        car_item.add_value("safety", car_info.get("Безпека"))
-        car_item.add_value("drive_unit", car_info.get("Привід"))
-        car_item.add_value("condition", car_info.get("Стан"))
-        car_item.add_value("transmission_type", car_info.get("Коробка передач"))
-        car_item.add_value("crash_info", car_info.get("Технічний стан"))
-        car_item.add_value("total_owners", car_info.get("Кількість власників"))
-        car_item.add_value("last_repair", car_info.get("Остання операція"))
-        car_item.add_value("has_crashes", has_crashes)
-        car_item.add_value("car_public_number", pb_number)
-        car_item.add_value("remains_at_large", remains_at_large)
-        car_item.add_value("is_VIN_confirmed", is_vin_number_confirmed)
-        car_item.add_value("VIN", vin_number)
-
-        # Information about seller
-        seller_phone_verified = bool(
-            response.xpath("(//div[@class='item_inner' and contains(., 'Перевірений банком')])[1]")
-        )
-        seller_verified_by_bank = bool(
-            response.xpath("(//span[@data-tooltip='Особистість продавця встановлена банком'])[1]")
-        )
-        seller_is_company = bool(
-            response.xpath(
-                "(//div[contains(@class, 'seller_info_title') and contains(., 'Компанія')])[1]"
-            )
-        )
-
-        # Information About Seller
-        seller_item = ItemLoader(item=CarSellerItem(), response=response)
-        seller_item.add_xpath("name", "(//h4[contains(@class, 'seller_info_name')])[1]//text()")
-        seller_item.add_xpath("last_online_time", "(//div[@id='lastVisit'])[1]/strong/text()")
-        seller_item.add_xpath(
-            "location",
-            "(//ul[contains(@class, 'checked-list')]/li)[1]/div/text()"
-        )
-        seller_item.add_xpath(
-            "signed_in_date",
-            # "(//span[contains(@data-tooltip, 'Продавець зареєстрований')])[1]/@data-tooltip"
-            "(//ul[contains(@class, 'checked-list')]/li)[3]//text()"
-        )
-        seller_item.add_xpath(
-            "reputation",
-            "(//div[@class='item_inner' and contains(., 'оцінка продавця')])[1]/span[@class='bold']/text()"
-        )
-        seller_item.add_xpath(
-            "company_location",
-            "(//div[@class='item_inner']/a[@class='map-loc']/text())[1]"
-        )
-        seller_item.add_xpath(
-            "sold_cars",
-            "(//div[@class='item_inner' and contains(., 'автомобілів')])[1]/text()"
-        )
-        seller_item.add_xpath(
-            "total_active_ads",
-            "(//div[@class='item_inner' and contains(., 'Пропозицій компанії')])[1]/a/text()"
-        )
-        seller_item.add_xpath(
-            "total_verified_active_ads",
-            "(//div[@class='item_inner' and contains(., 'Перевірених пропозицій')])[1]/a/text()"
-        )
-        seller_item.add_xpath(
-            "company_website",
-            "(//div[@class='item_inner' and contains(., 'Сайт компанії')])[1]/a/@href"
-        )
-        seller_item.add_value("verified_by_bank", seller_verified_by_bank)
-        seller_item.add_value("phone_verified", seller_phone_verified)
-        seller_item.add_value("is_company", seller_is_company)
-        loaded_car_item_item = car_item.load_item()
-        loaded_seller_item = seller_item.load_item()
-        yield {**loaded_car_item_item, ** loaded_seller_item}
+      Note: brand, model & year might be missing if the car was shipped
+            from Europe (peculiarity of the website).
+      """
+      # inspect_response(response, self)
+      brand_model_year = response.xpath("//span[@class='argument d-link__name']/text()")
+      if not brand_model_year:
+        self.logger.critical(f"Missing brand, model & year of the car in {response.url}")
+        return
+      brand, model, year = brand_model_year.get().strip().split(" ")
+      car = ItemLoader(item=Car(), response=response)
+      car.add_value("url", response.url)
+      car.add_value("brand", brand)
+      car.add_value("model", model)
+      car.add_value("year", year)
+      car.add_xpath("price", "//div[@class='price_value']/strong/text()")
+      item = car.load_item()
+      yield item
